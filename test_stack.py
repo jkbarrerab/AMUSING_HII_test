f2b1a7cf0423e736bbb3fb3222c21244b317b9a4
jkbarrerab <64662078+jkbarrerab@users.noreply.github.com>
2022-04-27 22:00:02 -0500

    catalogue of  emission lines

--- test_stack.py
+++ test_stack.py
@@ -5,6 +5,32 @@ import os, fnmatch
 from collections import Counter
 import re
 
-
 from astropy.table import QTable, Column, join, Table, vstack, hstack
 from matplotlib import pyplot as plt
+
+#path of files
+path_files = '/Users/jorgebarrera/Dropbox/Public/AMUSING/2022/HIIblob/cat_HII_pyHIIexplorer.220125/HII/HII_fe/'
+names = os.listdir(path_files)
+print(len(names))
+data =[]
+
+for i in range(len(names)):
+    converters = {'HIIREGID': [ascii.convert_numpy(str)]}
+    aux = Table.read(path_files+names[i], converters=converters)
+    data.append(aux)
+    print(i,'of', len(names))
+print('stacking individual files ...')    
+flux_cat = vstack(data)
+print('done...')
+# Adding Galaxy name column
+
+name_gal=[]
+print('adding name column ...')
+for i in flux_cat['HIIREGID']:
+        val_now = i.rstrip('1234567890')
+        name_gal.append(val_now.rstrip('-'))
+        
+flux_cat.add_column(name_gal, name='Name', index=1)        
+
+print('done...')
+
